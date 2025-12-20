85234bb38f30cf5593bb1fa90f4cfd54ef99f1ea
SFC EXAM <sfcexam@LABSFC.COM>
2025-12-20 10:48:18 +0500

    20

---
+++ ad.js/class 06/getter.js
@@ -0,0 +1,25 @@
+class Temperature{
+    #celsius;
+    constructor(celsius){
+        this.#celsius = value
+
+    }
+
+    get mytemp(){
+        return this.#celsius
+    }
+
+    set mytemp(value){
+        if(value > -20 && value <= 50){
+            this.#celsius = value;
+            document.write(`${this.#celsius} °C this is setter method <br>`)
+        }
+        else{
+            document.write(`invalid value <br>`)
+        }
+    }
+}
+
+let obj = new Temperature(50);
+document.write(obj.mytemp, " °C this is getter method <br>")
+obj.mytemp = 67;
