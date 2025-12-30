56a70aa033f2120b2d543dd8797c7e496b80d744
STUDENT <std1@LABSFC.COM>
2025-12-29 21:56:36 -0800

    30

---
+++ ad.js/class 10/new.js
@@ -0,0 +1,22 @@
+let status = document.getElementById("status")
+let result = document.getElementById("result")
+
+status.innerText = "order is pending......"
+let orderFood = new Promise((resolve, reject)=>{
+
+    let food = false;
+    setTimeout(function(){
+        if(food){
+            resolve('Food is ready')
+        }else{
+            reject('food is not ready')
+            
+        }
+    },3000)
+})
+orderFood.then((msg)=>{
+    status.innerText = msg
+    result.innerText = 'enjoy your meal'
+}).catch((error)=>{
+    status.innerText = error
+})
